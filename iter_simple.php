3f478fcc5d70989610c2dd73a622279a86a4acfa
macsly <macsly@ukr.net>
2018-07-12 16:19:34 +0300

    1-й коммит! Файл добавлен в репо gbook

---
+++ iter_simple.php
@@ -0,0 +1,17 @@
+<?php ## Стандартное поведение foreach.
+  class Monolog
+  {
+    public    $first  = "It's him.";
+    protected $second = "The Anomaly.";
+    private   $third  = "Do we proceed?";
+    protected $fourth = "Yes.";
+    private   $fifth  = "He is still...";
+    public    $sixth  = "...only human.";
+  }
+  $monolog = new Monolog();
+  echo "<h3>Выводим:</h3> <br />";
+  foreach ($monolog as $k => $v) {
+    echo "<br />$k: $v<br>";
+  }
+  echo "<h3>Конец вывода....</h3> <br />";
+?>
